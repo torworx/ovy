1bf3b3b1045626bed8b3ef9bb2fadba1ee246159
taoyuan <taoyuan@evymind.com>
2013-01-18 18:04:55 +0800

    Added index.js test.

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+var ovy = require('../index'),
+    assert = require('assert');
+
+it('ovy and plugins should exists', function() {
+    assert.ok(ovy);
+    assert.ok(ovy.arrays);
+    assert.ok(ovy.functions);
+})
